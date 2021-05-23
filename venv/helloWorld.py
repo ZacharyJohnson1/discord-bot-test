9c5fe6d8d3d1400917c301accfb44abc39e40fbd
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2021-05-23 18:36:06 -0500

    fix head conflict

--- venv/helloWorld.py
+++ venv/helloWorld.py
@@ -1,15 +1,8 @@
 import discord
 
 discord_client = discord.Client()
-<<<<<<< HEAD
 
 with open('token.txt') as token_file_client:
     token = token_file_client.readline(-1)
 
 discord_client.run(token)
-=======
-with open('token.txt') as token_file_client:
-    token = token_file_client.readlines()
-
-discord_client.run(token[0])
->>>>>>> 4ab96f1d306aa370cbbfa3eacc31a35c9157b737
