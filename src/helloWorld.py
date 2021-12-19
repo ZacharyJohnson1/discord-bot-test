b0ce0a9d85f2590b9d15517f32f670d6477cc7b1
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2021-12-19 16:40:08 -0600

    add on_reaction_add event for emojis

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,29 +1,43 @@
 import discord
+import re
 
 #create client
 discord_client = discord.Client()
 
-#event to debug ready state of bot
+'''event to debug ready state of bot'''
 @discord_client.event
 async def on_ready():
 
     print('bot is online')
 
 
-#event to handle messages addressed at bot
+'''event to handle messages addressed at bot'''
 @discord_client.event
 async def on_message(msg):
 
-    if msg.author == discord_client.user:
+    if msg.author == discord_client.user or msg == None:
         return
 
-    content = str(msg.content).lower()
-    if content.find('hello') or content.find('hi') or content.find('hey'):
+    user_msg_greeting = r'\bhello\b|\bhi\b|\bhey\b'
+    user_msg_content = str(msg.content).lower()
+
+    match = re.search(user_msg_greeting, user_msg_content)
+    if match:
         await msg.channel.send('Hello')
+        await msg.add_reaction('\U0001F44B')
 
 
-#read token from file
+'''echo user reactions'''
+@discord_client.event
+async def on_reaction_add(reaction, user):
+
+    print(reaction, user)
+    await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
+
+
+'''read token from file'''
 with open('token.txt') as token_file_client:
     token = token_file_client.readline(-1)
+
 #run client
 discord_client.run(token)
