f6f17c709f5a0087b04f4bc927a071861a029404
ZacharyJohnson1 <65374624+ZacharyJohnson1@users.noreply.github.com>
2021-12-19 16:59:28 -0600

    Create HelloWorld.py

---
+++ src/HelloWorld.py
@@ -0,0 +1,51 @@
+import discord
+import re
+
+#create client
+discord_client = discord.Client()
+
+'''event to debug ready state of bot'''
+@discord_client.event
+async def on_ready():
+
+    print('bot is online')
+
+
+'''event to handle messages addressed at bot'''
+@discord_client.event
+async def on_message(msg):
+
+    if msg.author == discord_client.user or msg == None:
+        return
+
+    user_msg_greeting = r'\bhello\b|\bhi\b|\bhey\b'
+    user_msg_content = str(msg.content).lower()
+
+    match = re.search(user_msg_greeting, user_msg_content)
+    if match:
+        await msg.channel.send('Hello')
+        await msg.add_reaction('\U0001F44B')
+
+
+'''echo user reactions'''
+@discord_client.event
+async def on_reaction_add(reaction, user):
+
+    await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
+
+
+'''track edited messages'''
+@discord_client.event
+async def on_message_edit(before, after):
+
+    #print(str(before.author, before.content, after.content))
+    await before.channel.send(before.content)
+
+
+
+'''read token from file'''
+with open('token.txt') as token_file_client:
+    token = token_file_client.readline(-1)
+
+#run client
+discord_client.run(token)
